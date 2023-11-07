a50eef82d858e723bbf53fa5248ee44fac7e9b04
ekachnwa <onyekachi.nwaogwu@gmail.com>
2023-11-06 20:26:13 -0800

    updating

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strstr - locates a substr
+ * @haystack: str to search
+ * @needle: substr to look for
+ * Return: pointer to beginning of substr or NULL not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, k;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (k = 0; needle[k] != '\0'; k++)
+		{
+			if (haystack[i + j] != needle[j])
+				break;
+		}
+		if (!needle[j])
+			return (&haystack[i]);
+	}
+	return (NULL);
+}
---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies the str src terminating null (\0)
+ * @dest: pointer to the buffer in which we copy the str
+ * @src: str to be copied
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int lenght = 0;
+
+	while (src[lenght] != '\0')
+	{
+		lenght++;
+	}
+
+	for (i = 0; i < lenght; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
