e42a37c03f3983183e8a7edc155a8545d44e6909
ekachnwa <onyekachi.nwaogwu@gmail.com>
2023-11-07 18:02:33 -0800

    updating dog.h file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,18 +1,30 @@
-#include <stdlib.h>
-#include "dog.h"
+#ifndef DOG_H
+#define DOG_H
 
 /**
- * init_dog - initializes a var dog
- * @d: pointer to  dog
- * @name: name
- * @age: age
- * @owner: owner
+ * struct dog - dog's info
+ * @name: 1st member
+ * @age: 2nd member
+ * @owner: 3rd member
+ * Description: Longer description
  */
-void init_dog(struct dog *d, char *name, float age, char *owner)
+struct dog
 {
-	if (d == NULL)
-		d = malloc(sizeof(struct dog));
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
-}
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
+#endif
