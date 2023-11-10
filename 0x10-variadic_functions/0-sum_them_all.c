3c0bc55f4ecf5c80b6899b16d3cfd1aa903c90ed
ekachnwa <onyekachi.nwaogwu@gmail.com>
2023-11-09 18:36:53 -0800

    update

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - calculates the sums
+ * @n: number of arguments passed
+ * Return: result sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sums = 0;
+	va_list list;
+
+	va_start(list, n);
+
+	for (i = 0; i < n; i++)
+		sums += va_arg(list, int);
+
+	va_end(list);
+
+	return (sums);
+}
