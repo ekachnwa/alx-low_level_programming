d4c4091d378cc18d3415e7c45d4a4a333ff85731
ekachnwa <onyekachi.nwaogwu@gmail.com>
2023-11-20 05:22:18 -0800

    updating task 2

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements
+ * @h: linked list
+ * Return: number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t numb = 0;
+
+	while (h)
+	{
+		numb++;
+		h = h->next;
+	}
+
+	return (numb);
+}
